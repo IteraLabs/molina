ce3b8a42efe3982a37f4d398aa3e8ba02351f292
IFFranciscoME <if.francisco.me@gmail.com>
2024-10-02 11:58:12 -0600

    first publishable version

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,23 +1,30 @@
-//! Molina, in honor to THE greatest Mexican researcher of all times, Mario Molina (1943-2020).
+//! # Molina
+//! 
+//! *In honor to THE greatest Mexican researcher of all times, Mario Molina (1943-2020).
 //!
-//! A Rust and Python Synthetic Integration for an agentic-LLM approach to build a research agent
+//! A Rust and Python Synthetic Integration for an agentic-LLM approach to build research agents
 //! for local knowledge representation.
+//!
+//! - **Python**: A wrapper of core functionality in Rust, Interface to models.
+//! - **Rust**: NLP tasks, Prompting, Routing, Content parsing.
+//!
 
 use crate::content::parse;
 use pyo3::prelude::*;
 
-/// Tools for content parsing and generation
+/// Content parsing for PDFs and other tools.
 pub mod content;
 
-/// Tools for data accessing, I/O, compression.
+/// Data I/O processes.
 pub mod data;
 
-/// Tools for making an inference computation
+/// Model's inference engine and related computations.
 pub mod inference;
 
-/// Events, Custom Error Types, Logs
+/// Structs and logic for Events, Custom Error Types, Logs
 pub mod messages;
 
+/// testing docs.rs element for #[pyfunction]
 #[pyfunction]
 fn read_pdf(_file_path: &str) -> PyResult<()> {
     let read_result = parse::pdf_generate();
---
+++ rust/tests/pyo3_integration.rs
@@ -0,0 +1,8 @@
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn rs_functions() {
+        assert_eq!(5, 5);
+    }
+}
