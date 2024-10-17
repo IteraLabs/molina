5bc09f391df496e9fba8df7154dcf1d01745323f
IFFranciscoME <if.francisco.me@gmail.com>
2024-10-16 22:22:02 -0600

    From loading, extraction and tokenization progress

--- rust/examples/extract_content.rs
+++ rust/examples/extract_content.rs
@@ -1,13 +1,16 @@
 use molina::content::extract;
-// use tokenizers::tokenizer::{Tokenizer, Result};
-// use tokenizers::models::bpe::BPE;
 
-fn main() {
+use std::error::Error;
+use llm_models::tokenizer::LlmTokenizer;
+use std::path::PathBuf;
+
+fn main() -> Result<(), Box<dyn Error>> {
+
     let wd_folder: String = "/Users/franciscome/git/iteralabs/molina".to_owned();
     let in_folder: &str = "/knowledge";
     let in_subfolder: &str = "/conference_icml";
     let in_file: &str = "/alon22a.pdf";
-    let in_path = wd_folder + in_folder + in_subfolder + in_file;
+    let in_path = wd_folder.clone() + in_folder + in_subfolder + in_file;
     
     // -- ------------------------------------------------------ CONTENT EXTRACTION -- //
     // -- ------------------------------------------------------ ------------------ -- //
@@ -19,27 +22,22 @@ fn main() {
     
     // -- ------------------------------------------------------------ TOKENIZATION -- //
     // -- ------------------------------------------------------------ ------------ -- //
+    
+    let in_tok = wd_folder.clone() + "/models/Meta-Llama-3-8B-Instruct/tokenizer.json";
+    let path_buf = PathBuf::from(in_tok);
 
-    // let tokenized_doc = &raw_document[&2];
-    // let tokens = tokenizer.encode(tokenized_doc, false)?;
+    let llama_tokenizer = LlmTokenizer::new_from_tokenizer_json(&path_buf)?;
+    let llama_tokens = llama_tokenizer.tokenize("This is a sentence");
 
-    //println!("Token Count: {}", tokens.len());
-    //println!("Tokens: {:?}", tokens);
+    println!("\nValidation: Token count: {}", llama_tokens.len());
+    println!("Validation: Downloaded meta/llama3 {:?}\n", llama_tokens);
 
-    // Print the raw contents, structured as indicated in the docs
-    // https://docs.rs/lopdf/latest/lopdf/struct.Document.html
-    // let obj_document: Vec<_> = raw_document.objects.clone().into_values().collect();
-    // println!("The objects that compose the PDF: {:?}", obj_document);
+    let tokenized_doc = &raw_document[&2];
+    let tokens_doc = llama_tokenizer.tokenize(tokenized_doc);
 
-    // Get all the keys
-    // println!("No. of Keys is: {:?}", &first_text.text.keys().len());
+    println!("\nContent Len: {}", tokenized_doc.len());
+    println!("Tokenized Content Len: {}", tokens_doc.len());
+    println!("Actual Tokens: {:?}\n", tokens_doc);
 
-    // Return the first K, V pair
-    // println!("First K,V \n{:?}", &first_text.text.first_key_value());
-
-    // Return the last K, V pair
-    //println!("Last K,V \n{:?}", &first_text.text.last_key_value());
-
-    // for a given key, get the content
-    // println!("Content {:?}", &first_text.text.get(&26));
+    Ok(())
 }
